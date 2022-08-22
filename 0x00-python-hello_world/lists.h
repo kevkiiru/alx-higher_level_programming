15a7379b9ba18d2b818a2f4c74ba5125fc4cdb3f
kevkiiru <kevkiiru6@gmail.com>
2022-08-23 01:34:12 +0300

    the lists header file

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -18,7 +18,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+size_t print_listint(const listint_t *h);
 int check_cycle(listint_t *list);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
 
 #endif
