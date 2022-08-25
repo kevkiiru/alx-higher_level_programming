3ea2c473e443aedf9ab71f417e38050a61a2c940
kevkiiru <kevkiiru6@gmail.com>
2022-08-25 21:32:24 +0300

    the variable load file

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    def variable_load():
-        for a in range():
+    def variable_load(num):
+        for a in range(num):
             print("{}".format(a))
     variable_load()
