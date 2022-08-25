4fda0be0ae121a4610cc7d5407d6cae380886d27
kevkiiru <kevkiiru6@gmail.com>
2022-08-25 20:03:00 +0300

    the add py file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def add(a, b):
+if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    print("(a {} + b {} = add{:a}, {:b} {}.format(a, b, a + b)")
-    
-add()
+    print("{} + {} = {}".format(a, b, add(a, b)))
