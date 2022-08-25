e3b372c09d7d01b11a12522bd03fcc4329b7b2d7
kevkiiru <kevkiiru6@gmail.com>
2022-08-25 22:22:33 +0300

    the calculator file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
     b = int(sys.argv[3])
     
     if op is '+':
-        print("{} {} {} = {}".format(a, op, b, add(a, b)))
+        print("{} {} = {}".format(a, op, b, add(a, b)))
     elif op is '-':
         print("{} {} = {}".format(a, op, b, sub(a, b)))
     elif op is '*':
-        print("{} {} = {}".format(a, op, b(a, b)))
+        print("{} {} = {}".format(a, op, b, mul(a, b)))
     elif op is '/':
-        print("{} {} = {}".format(a, op, b(a, b)))
+        print("{} {} = {}".format(a, op, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
