809cb7afecbde14ede1af444717579ea1d4b42e1
kevkiiru <kevkiiru6@gmail.com>
2022-08-26 00:39:58 +0300

    the calculator file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
         a = int(a)
         b = int(b)
         if op == '+':
-            print("{a} + {b} = {calculator_1.add(a, b)}")
+            print(f"{a} + {b} = {calculator_1.add(a, b)}")
         elif op == '-':
-            print("{a} - {b} = {calculator_1.sub(a, b)}")
+            print(f"{a} - {b} = {calculator_1.sub(a, b)}")
         elif op == '*':
-            print("{a} * {b} = {calculator_1.mul(a, b)}")
+            print(f"{a} * {b} = {calculator_1.mul(a, b)}")
         elif op == '/':
-            print("{a} / {b} = {calculator_1.div(a, b)}")
+            print(f"{a} / {b} = {calculator_1.div(a, b)}")
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
