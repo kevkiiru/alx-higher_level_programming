8a01a403ed6ba2628f6855a302a1e8a20a8976d4
kevkiiru <kevkiiru6@gmail.com>
2022-08-26 00:38:00 +0300

    the calculator file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,17 +5,18 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a, op, b = argv[1:]
-    a = int(a)
-    b = int(b)
-    if op == '+':
-        print("{a} + {b} = {calculator_1.add(a, b)}")
-    elif op == '-':
-        print("{a} - {b} = {calculator_1.sub(a, b)}")
-    elif op == '*':
-        print("{a} * {b} = {calculator_1.mul(a, b)}")
-    elif op == '/':
-        print("{a} / {b} = {calculator_1.div(a, b)}")
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        a, op, b = argv[1:]
+        a = int(a)
+        b = int(b)
+        if op == '+':
+            print("{a} + {b} = {calculator_1.add(a, b)}")
+        elif op == '-':
+            print("{a} - {b} = {calculator_1.sub(a, b)}")
+        elif op == '*':
+            print("{a} * {b} = {calculator_1.mul(a, b)}")
+        elif op == '/':
+            print("{a} / {b} = {calculator_1.div(a, b)}")
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
