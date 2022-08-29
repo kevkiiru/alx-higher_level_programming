c1623b88cbf5829723f1015421af235dcea9cf0a
kevkiiru <kevkiiru6@gmail.com>
2022-08-29 21:07:04 +0300

    the element at file

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for idx in range(len(my_list)):
+        if idx < my_list:
+            return (None)
+        else:
+            return (None)
