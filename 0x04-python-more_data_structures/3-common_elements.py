2c79741f7fc36555b8d3638b2054c6039e9c1ac7
kevkiiru <kevkiiru6@gmail.com>
2022-08-31 21:12:31 +0300

    the common elements file

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,10 +2,4 @@
 
 def common_elements(set_1, set_2):
 
-    setOne = set(set_1)
-    setTwo = set(set_2)
-
-    if (setOne & setTwo):
-        print(setOne & setTwo)
-    else:
-        print("No common")
+    return set_1 & set_2
