8f84d35210a245892f324bbe2be3c61fa64c8873
kevkiiru <kevkiiru6@gmail.com>
2022-09-15 23:31:58 +0300

    square 4 file

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """Represents a square
+
+    Attributes:
+        _size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        """initializes the square
+
+        Args:
+            size (int): size of a side of the square
+
+        Returns:
+            None
+        """
+
+    def area(self):
+        """calculates the square's area
+
+        Returns:
+            The area of the square
+        """
+        return (self._size) ** 2
+
+    @property
+    def size(self):
+        """getter of _size
+
+        Returns:
+            the size of the square
+        """
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        """setter of _size
+
+        Args:
+            value (int): the size of a size of the square
+
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self._size = value
