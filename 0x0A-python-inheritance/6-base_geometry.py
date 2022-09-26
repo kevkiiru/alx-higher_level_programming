4f559d58e3993d80abca8db99c7d57aefa69eea1
kevkiiru <kevkiiru6@gmail.com>
2022-09-26 22:22:36 +0300

    base geometry 2 file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module base_geometry. Class geometry
+"""
+
+
+class BaseGeometry:
+    """The class base_geometry"""
+
+    def area(self):
+        """To raise an exception with a message
+
+        Args:
+            area: the exception
+
+        Returns: the area of the geometry
+        """
+
+        raise Exception('area() is not implemented')
