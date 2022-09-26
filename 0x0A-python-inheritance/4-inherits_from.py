92e122e8409226a18afa3a81f2f00510c35f37ee
kevkiiru <kevkiiru6@gmail.com>
2022-09-26 22:08:58 +0300

    inherits from file

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module inherits_from. If object is an instance of a class that inherited,
+directly or not, from specified class.
+"""
+
+
+def inherits_from(obj, a_class):
+    """Determines if obj is an instance of a class that inherited from a_class
+
+    Args:
+        obj: object to look at
+        a_class: class to evaluate
+
+    Returns: True or False
+    """
+
+    return isinstance(obj, a_class) and type(obj) != a_class
