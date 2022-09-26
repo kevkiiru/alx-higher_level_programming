5d0d520e2e75bffa4531f908d9f69b551a73b9d0
kevkiiru <kevkiiru6@gmail.com>
2022-09-26 23:23:19 +0300

    rectangle file

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module rectangle. Initializes instation
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle. Private instance attributes of width and height
+    Inherits from BaseGeometry.
+    """
+
+    def __init__(self, width, height):
+        """Initializes an instance of width and height of the rectangle"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self._width = width
+        self._height = height
