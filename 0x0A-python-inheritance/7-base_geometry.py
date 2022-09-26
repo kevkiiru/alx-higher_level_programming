9d2ee3737530cf6951ec1906c18a8baca9838cd1
kevkiiru <kevkiiru6@gmail.com>
2022-09-26 23:13:32 +0300

    base geometry file

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module base_geometry. Finding public instance methods.
+"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry."""
+
+    def area(self):
+        """area of the geometry"""
+
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """value: the one to validated"""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise TypeError("{} must be greater than 0".format(name))
