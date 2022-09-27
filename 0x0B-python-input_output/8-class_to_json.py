6c56392f5784d55cb89bc9e6cdb996d5e02ebfde
kevkiiru <kevkiiru6@gmail.com>
2022-09-28 01:06:43 +0300

    class to json file

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,9 +3,6 @@
 """
 
 
-import json
-
-
 def class_to_json(obj):
     """returns the dictionary desctiption with simple data structure(list,
     dictionary, string, integer and boolean) for JSON serialization of an
