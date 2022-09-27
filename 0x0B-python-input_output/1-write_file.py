88f27a0c0fe154fa8a29f05ef195b6b3677147a4
kevkiiru <kevkiiru6@gmail.com>
2022-09-28 00:22:26 +0300

    write file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,6 +9,5 @@ def write_file(filename="", text=""):
         filename: name of the file
         text: string to write in the file
     """
-
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
