8b3bc0be1f73a0c871d90cc3b5f6b73e2b43e168
kevkiiru <kevkiiru6@gmail.com>
2022-11-03 00:17:14 +0300

    top score file

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,2 @@
 -- lists all the records of the table second_table of the database
-SELECT 'score', 'name'
-FROM 'second_table'
-ORDER BY 'score' DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
