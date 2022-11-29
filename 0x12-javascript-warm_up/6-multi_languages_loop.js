8b88353516a252d1dbf0f79c9b206a9fff0e3850
kevkiiru <kevkiiru6@gmail.com>
2022-11-30 00:09:35 +0300

    6-multi_languages_loop file

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const argv = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const argv = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const arg in argv) {
   console.log(argv[arg]);
 }
