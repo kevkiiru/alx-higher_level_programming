b3f1682c165743d42742df9169e45405abc61e50
kevkiiru <kevkiiru6@gmail.com>
2022-11-29 23:23:19 +0300

    1-multi_languages file

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const line1 = 'C is fun';
-const line2 = 'Python is cool';
-const line3 = 'JavaScript is amazing';
-console.log(line1);
-console.log(line2);
-console.log(line3);
+const lineOne = 'C is fun';
+const lineTwo = 'Python is cool';
+const lineThree = 'JavaScript is amazing';
+console.log(lineOne);
+console.log(lineTwo);
+console.log(lineThree);
