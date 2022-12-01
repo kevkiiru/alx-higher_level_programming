9d6522126b783dddb7c034825debc9e35c95e005
kevkiiru <kevkiiru6@gmail.com>
2022-12-01 14:45:29 +0300

    5-square file

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// class square
+
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Rectangle;
+module.exports = Square;
