9d3e429966f405c8b64a5ef7205958efc3860144
kevkiiru <kevkiiru6@gmail.com>
2022-12-01 15:05:20 +0300

    9-logme file

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.logMe = function (item) {
+  if (this.count === undefined) {
+    this.count = 0;
+  }
+  function argPrinter () {
+    console.log(`${this.count}: ${item}`);
+    this.count += 1;
+  }
+  return argPrinter();
+};
