6441aa2be27883c262110e19e0cdc7d2e60a2b2e
kevkiiru <kevkiiru6@gmail.com>
2022-12-01 14:29:30 +0300

    2-rectangle file

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// class rectangle
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+};
