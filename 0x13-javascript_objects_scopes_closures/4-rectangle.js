b3b9cd6dcfa2dce247f108c4fa2369c483d33209
kevkiiru <kevkiiru6@gmail.com>
2022-12-01 14:42:24 +0300

    4-rectangle file

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+// class rectangle
+
+const Rectangle = class {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      w = undefined;
+      h = undefined;
+    }
+    if (w !== undefined && h !== undefined) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const printer = 'X'.repeat(this.width);
+    for (let a = 0; a < this.height; a++) {
+      console.log(printer);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
+
+module.exports = Rectangle;
