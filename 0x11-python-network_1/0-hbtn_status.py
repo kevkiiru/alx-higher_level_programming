90accd0b7d8d0c6f55a470a43407fd54315110b0
kevkiiru <kevkiiru6@gmail.com>
2022-12-20 17:39:46 +0300

    hbtn status file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -14,7 +14,6 @@ def get_status():
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         return response.read()
 
-
 if __name__ == "__main__":
     # Get the status and print the body of the response
     status = get_status()
